a5c50590d7dfa476a65e3784fcfd795e593c4d69
sanjay <mahatosanjay207@gmail.com>
2025-09-08 20:25:52 +0530

    changes

--- app/(admin)/admin/layout.tsx
+++
@@ -1,5 +0,0 @@
-import ClientAdminLayout from "@/components/admin/clientAdminlayout";
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <ClientAdminLayout>{children}</ClientAdminLayout>;
-}
